c33a6d02c8c1fbd5f14482c21bdf21f2f84bc2c2
lol <wtflmao@126.com>
2022-07-23 04:42:13 +0800

    commit

--- commands/wordCollector.js
+++ commands/wordCollector.js
@@ -8,26 +8,25 @@ module.exports = {
 		interaction.reply("type discord");
 
 		const filter = m => {
-			return m.content.toLowerCase().includes('discord') && m.author.id === interaction.user.id
+			return (m.content.toString().toLowerCase().includes('discord')) && (m.author.id === interaction.user.id)
 		} // m is a message object that will be passed through the filter function
-		console.log(interaction.user.id);
+		//console.log(interaction.user.id);
 
 		const collector = interaction.channel.createMessageCollector({
 			filter,
 			max: 1,
 			time: 1000 * 10, //10 secs
 		})
-		//interaction.deferReply();
+
 		collector.on('collect', m => {
 			console.log(`Collected from ${m.author.id}`);
 			m.reply("Yes!");
 		});
 
-		console.log("here333");
 		collector.on('end', collected => {
 			if (collected.size === 0) {
-				interaction.channel.send(`<@${interaction.user.id}> You did not type discord.`)
-					.then(message => console.log(`Sent message: <@${interaction.user.id}> You did not type discord.`))
+				interaction.channel.send(`<@${interaction.user.id}> You did not type word discord.`)
+					.then(message => console.log(`Sent message:\n<@${interaction.user.id}> You did not type discord.`))
 					.catch(console.error);
 				return
 			}
