562aa955f1ed3430e293be6fd1cd9ba9a2fc2d65
lol <wtflmao@126.com>
2022-11-17 13:45:18 +0800

    Event handling and autocomplete interaction handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,42 @@ const { Events } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
-	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	async execute(interaction) {
+		
+		if (interaction.isChatInputCommand()) {
+
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+
+			try {
+				await command.execute(interaction);
+				console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an autocomplete response.`);
+			} catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
+
+		} else if (interaction.isAutocomplete()) {
+
+			const command = interaction.client.commands.get(interaction.commandName);
+
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+
+			try {
+				await command.autocomplete(interaction);
+			} catch (error) {
+				console.error(error);
+			}
+
+		} else {
+			// not a command
+		}
 	},
 };
--- index.js
+++ index.js
@@ -40,25 +40,6 @@ for (const fileArray of commandFiles) {
 }
 
 client.once(Events.ClientReady, () => {});
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({
-            content: 'There was an error while executing this command!',
-            ephemeral: true
-        });
-    }
-});
+client.on(Events.InteractionCreate, () => {});
 
 client.login(token);
